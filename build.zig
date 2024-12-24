062c5360de423f46bc3c6e1b899209a14a3aa331
grantwill74 <22186487+grantwill74@users.noreply.github.com>
2024-12-24 10:35:09 -0800

    update build.zig

--- build.zig
+++ build.zig
@@ -1,15 +1,19 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const exe = b.addExecutable(.{
+    const dbg = b.addExecutable(.{
         .name = "nim",
         .root_source_file = b.path("nim.zig"),
         .target = b.graph.host,
+        .optimize = .Debug,
+        .link_libc = false,
+        .linkage = .static,
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(dbg);
+    
 
-    const run_exe = b.addRunArtifact(exe);
+    const run_exe = b.addRunArtifact(dbg);
 
     const run_step = b.step("run", "Play nim.");
     run_step.dependOn(&run_exe.step);
